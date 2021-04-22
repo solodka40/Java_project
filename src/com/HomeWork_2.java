d2ef4a79fada5ac9595546bf09230741259d613c
Alena Andreeva <solodka40@gmail.com>
2021-04-22 16:13:17 +0300

    1. Создан новый класс HomeWork_2. 2. Объявлена переменная num_1 типа double, ей присвоено произвольное дробное значение. 3. Объявлена переменная типа byte, ей присвоено максимально допустимое значение.

---
+++ src/com/HomeWork_2.java
@@ -0,0 +1,9 @@
+package com;
+public class HomeWork_2 {
+    public static void main(String[] args) {
+        double num_1=231.765;
+        byte num_2=127;
+        System.out.println(num_1);
+        System.out.println(num_2);
+    }
+}
