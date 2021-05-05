5036c65cf205325d25ec4c701998efe830d035ef
Alena Andreeva <solodka40@gmail.com>
2021-05-05 23:02:32 +0300

    Задача №8_ Циклы

---
+++ src/com/HomeWork8.java
@@ -0,0 +1,20 @@
+package com;
+import java.util.Scanner;
+public class HomeWork8 {
+    public static void main(String[] args) {
+        int an,k;
+        //int [] arr;
+        Scanner q = new Scanner(System.in);
+        System.out.println("Введите любое целое положительное число:");
+        an = q.nextInt();
+        int sum=0;
+        for (int c=1;c<an;c++) {
+            if (c % 2 == 0)
+                continue;
+            sum = sum + c;
+            c += 1;
+
+        }
+        System.out.println("Сумма всех нечетных чисел от 1 до введенного Вами числа равна:"+sum);
+        }
+    }
